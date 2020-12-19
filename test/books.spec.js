caa7c141800dbf912f3989b7fb69e6d1e5fae2ec
Mohammed Islam <mmislam101@gmail.com>
2020-12-19 13:27:23 -0700

    Add more pagination tests

--- test/books.spec.js
+++ test/books.spec.js
@@ -38,7 +38,7 @@ before(async () => {
 
 describe('Books', () => {
 
-    it('Defaults to 5 books without page information', async () => {
+    it('defaults to 5 books without page information', async () => {
         // The default limit is set in the manifest for hapi-pagination
 
         const books = await server.inject({
@@ -59,5 +59,31 @@ describe('Books', () => {
         expect(books.result.results[0].type).to.equal(Constants.book_type.quran);
 
         expect(books.result.results[4].type).to.equal(Constants.book_type.zikr);
+    });
+
+    it('paginates through books properly', async () => {
+
+        const books = await server.inject({
+            method: 'get',
+            url: '/api/books?page=2&limit=3',
+            headers: {
+                authorization: access,
+                'x-api-key': process.env.API_KEY
+            }
+        });
+
+        expect(books.statusCode).to.equal(200);
+
+        expect(books.result.results.length).to.equal(3);
+
+        expect(books.result.meta.count).to.equal(3);
+        expect(books.result.meta.totalCount).to.equal(11);
+        expect(books.result.meta.pageCount).to.equal(4);
+
+        expect(books.result.results[0].slug_id).to.equal('book-zikr-surah-ikhlas');
+        expect(books.result.results[0].type).to.equal(Constants.book_type.zikr);
+
+        expect(books.result.results[2].slug_id).to.equal('book-zikr-alhamdu');
+        expect(books.result.results[2].type).to.equal(Constants.book_type.zikr);
     });
 });
