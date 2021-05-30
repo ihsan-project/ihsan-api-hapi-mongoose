346ccb4fb7576e5930f0514bd5e222e787667c9d
Mohammed Islam <mmislam101@gmail.com>
2021-05-30 09:55:11 -0700

    Fix get books pagination

--- seeds/books/index.js
+++ seeds/books/index.js
@@ -4,57 +4,67 @@ const Constants = require('../../lib/constants');
 
 module.exports = [
     {
+        order: 1,
         slugId: 'book-quran',
         type: Constants.book_type.quran,
         title: 'Quran'
     },
     {
+        order: 2,
         slugId: 'book-zikr-subh-allah',
         type: Constants.book_type.zikr,
         title: 'Subhaan Allaahi'
     },
     {
+        order: 3,
         slugId: 'book-zikr-surah-fatiha',
         type: Constants.book_type.zikr,
         title: 'Surah Fatiha'
     },
     {
+        order: 4,
         slugId: 'book-zikr-surah-ikhlas',
         type: Constants.book_type.zikr,
         title: 'Surah Ikhlas'
     },
     {
+        order: 5,
         slugId: 'book-zikr-la-ilaha',
         type: Constants.book_type.zikr,
         title: 'La Ilaha'
     },
     {
-        id: 6,
+        order: 6,
         slugId: 'book-zikr-alhamdu',
         type: Constants.book_type.zikr,
         title: 'Alhamdulillah'
     },
     {
+        order: 7,
         slugId: 'book-zikr-allahuakbar',
         type: Constants.book_type.zikr,
         title: 'Allahuakbar'
     },
     {
+        order: 8,
         slugId: 'book-salawaat-al-fatih',
         type: Constants.book_type.salawaat,
         title: 'Salawat Al-Fatih'
     },
     {
+        order: 9,
         slugId: 'book-salawaat-munajiyyah',
         type: Constants.book_type.salawaat,
         title: 'Salatu Munajiyyah'
     },
     {
+        order: 10,
         slugId: 'book-salawaat-ali',
         type: Constants.book_type.salawaat,
         title: 'Salawaat of Sayyidina Ali '
     },
     {
+        order: 11,
         slugId: 'book-salawaat-sayyid',
         type: Constants.book_type.salawaat,
         title: 'Sayyid as-Salawaat'
